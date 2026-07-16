c536499ad1895f0c01c164635fd6af58831faace
Jhostin Cadena <150101200+jhosnit@users.noreply.github.com>
2026-07-15 22:27:27 -0500

    Refactor update method in PersonaDAO
    Updated the update method to replace the entire Persona object instead of updating fields individually.

--- Persona/src/main/java/modelo/dao/PersonaDAO.java
+++ Persona/src/main/java/modelo/dao/PersonaDAO.java
@@ -10,17 +10,16 @@ public class PersonaDAO {
 	public void create(Persona p) {
 		personas.add(p);
 	}
-
+	
 	public void update(Persona p) {
-		for (int i = 0; i < personas.size(); i++) {
-			if (personas.get(i).getCedula().equals(p.getCedula())) {
-				personas.get(i).setCedula(p.getCedula());
-				personas.get(i).setNombre(p.getNombre());
-				personas.get(i).setEdad(p.getEdad());
+		for(int i = 0; i < personas.size(); i++) {
+			if(personas.get(i).getId() == p.getId()) {
+				personas.set(i, p);
+				break;
 			}
 		}
 	}
-
+	
 	public void delete(int id) {
 		for (int i = 0; i < personas.size(); i++) {
 			if (personas.get(i).getId() == id) {
